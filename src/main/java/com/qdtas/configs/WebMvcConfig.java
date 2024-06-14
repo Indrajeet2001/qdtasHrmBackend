d2daa9b1355eaa5294fe7f3b7b2f00b15220a51b
Indrajeet_kedari <indrajeetkedari421@gmail.com>
2024-06-14 17:04:35 +0530

    first commit

--- src/main/java/com/qdtas/configs/WebMvcConfig.java
+++ src/main/java/com/qdtas/configs/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.qdtas.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,4 +13,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
                 .resourceChain(false);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Adjust this according to your front-end URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
